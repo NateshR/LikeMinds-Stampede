b8df53c8ba223b245c18cff84281a4fcf9e50cfa
Mahir-G2 <mahir.gupta@likeminds.community>
2023-10-06 13:03:47 +0530

    Testing conditional code - update 30

--- kettle.tf
+++ kettle.tf
@@ -78,7 +78,7 @@ resource "kubernetes_ingress_v1" "kettle-load" {
     annotations = {
       "kubernetes.io/ingress.class": "gce"
       "kubernetes.io/ingress.allow-http": "true"
-      "kubernetes.io/ingress.global-static-ip-name": "likeminds-nonprod-caravan-celery-static-ip"
+      "kubernetes.io/ingress.regional-static-ip-name": "kettle-load-testing-static-ip"
     }
   }
 
